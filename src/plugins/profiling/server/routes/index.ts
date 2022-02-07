7282d0971fb31e8eed32472d82159b0fdc15ec8c
inge4pres <francesco.gualazzi@elastic.co>
2022-02-07 12:30:39 +0100

    remove b64 encoding function, retrieve IDs from ES

--- src/plugins/profiling/server/routes/index.ts
+++ src/plugins/profiling/server/routes/index.ts
@@ -6,17 +6,3 @@
  * Side Public License, v 1.
  */
 export { registerRoutes } from './register_routes';
-
-export function getBase64Encoding(hex: string) {
-  const bin = [];
-  let i = 0;
-  let d;
-  let b;
-  while (i < hex.length) {
-    d = parseInt(hex.slice(i, i + 2), 16);
-    b = String.fromCharCode(d);
-    bin.push(b);
-    i += 2;
-  }
-  return btoa(bin.join('')).replace('+', '-').replace('/', '_');
-}
