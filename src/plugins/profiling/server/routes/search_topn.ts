c6d7151f2a6bab5d6a84a13ae78ccaf306c31320
inge4pres <francesco.gualazzi@elastic.co>
2022-02-09 16:31:20 +0100

    topn: use default ES client
    Signed-off-by: inge4pres <francesco.gualazzi@elastic.co>

--- src/plugins/profiling/server/routes/search_topn.ts
+++ src/plugins/profiling/server/routes/search_topn.ts
@@ -7,7 +7,6 @@
  */
 import { schema } from '@kbn/config-schema';
 import type { IRouter } from 'kibana/server';
-import { IEsSearchRequest } from '../../../data/server';
 import { IEsSearchResponse } from '../../../data/common';
 import type { DataRequestHandlerContext } from '../../../data/server';
 import { getRemoteRoutePaths } from '../../common';
@@ -33,57 +32,54 @@ export function queryTopNCommon(
       const { index, projectID, timeFrom, timeTo } = request.query;
 
       try {
-        const resTopNStackTraces = await context
-          .search!.search(
-            {
-              params: {
-                index,
-                body: {
-                  query: {
-                    bool: {
-                      must: [
-                        {
-                          term: {
-                            ProjectID: {
-                              value: projectID,
-                              boost: 1.0,
-                            },
-                          },
+        const esClient = context.core.elasticsearch.client.asCurrentUser;
+        const resTopNStackTraces = await esClient.search(
+          {
+            index,
+            body: {
+              query: {
+                bool: {
+                  must: [
+                    {
+                      term: {
+                        ProjectID: {
+                          value: projectID!,
+                          boost: 1.0,
                         },
-                        {
-                          range: {
-                            '@timestamp': {
-                              gte: timeFrom,
-                              lt: timeTo,
-                              format: 'epoch_second',
-                              boost: 1.0,
-                            },
-                          },
-                        },
-                      ],
+                      },
                     },
+                    {
+                      range: {
+                        '@timestamp': {
+                          gte: timeFrom,
+                          lt: timeTo,
+                          format: 'epoch_second',
+                          boost: 1.0,
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+              aggs: {
+                histogram: {
+                  auto_date_histogram: {
+                    field: '@timestamp',
+                    buckets: 100,
                   },
                   aggs: {
-                    histogram: {
-                      auto_date_histogram: {
-                        field: '@timestamp',
-                        buckets: 100,
+                    group_by: {
+                      terms: {
+                        field: searchField,
+                        order: {
+                          Count: 'desc',
+                        },
+                        size: 100,
                       },
                       aggs: {
-                        group_by: {
-                          terms: {
-                            field: searchField,
-                            order: {
-                              Count: 'desc',
-                            },
-                            size: 100,
-                          },
-                          aggs: {
-                            Count: {
-                              sum: {
-                                field: 'Count',
-                              },
-                            },
+                        Count: {
+                          sum: {
+                            field: 'Count',
                           },
                         },
                       },
@@ -91,20 +87,21 @@ export function queryTopNCommon(
                   },
                 },
               },
-            } as IEsSearchRequest,
-            {}
-          )
-          .toPromise();
+            },
+          },
+          {}
+        );
 
         if (searchField === 'StackTraceID') {
           const docIDs: string[] = [];
-          resTopNStackTraces.rawResponse.aggregations.histogram.buckets.forEach((timeInterval) => {
-            timeInterval.group_by.buckets.forEach((stackTraceItem: any) => {
-              docIDs.push(stackTraceItem.key);
+          const autoDateHistogram = resTopNStackTraces.body.aggregations?.histogram!;
+          if ('buckets' in autoDateHistogram) {
+            autoDateHistogram.buckets.forEach((timeInterval) => {
+              timeInterval.group_by.buckets.forEach((stackTraceItem: any) => {
+                docIDs.push(stackTraceItem.key);
+              });
             });
-          });
-
-          const esClient = context.core.elasticsearch.client.asCurrentUser;
+          }
 
           const resTraceMetadata = await esClient.mget<any>({
             index: 'profiling-stacktraces',
@@ -113,14 +110,14 @@ export function queryTopNCommon(
 
           return response.ok({
             body: {
-              topN: (resTopNStackTraces as IEsSearchResponse).rawResponse.aggregations,
+              topN: resTopNStackTraces.body.aggregations,
               traceMetadata: resTraceMetadata.body.docs,
             },
           });
         } else {
           return response.ok({
             body: {
-              topN: (resTopNStackTraces as IEsSearchResponse).rawResponse.aggregations,
+              topN: resTopNStackTraces.body.aggregations,
             },
           });
         }
