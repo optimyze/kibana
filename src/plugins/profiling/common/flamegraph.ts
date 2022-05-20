03beda4e0108e6d30190a525496aad7cfc39e171
Joseph Crail <joseph.crail@elastic.co>
2022-05-20 14:14:39 -0700

    Use new WebGL-rendered Elastic flamegraph

--- src/plugins/profiling/common/flamegraph.ts
+++ src/plugins/profiling/common/flamegraph.ts
@@ -22,19 +22,104 @@ import {
   groupStackFrameMetadataByStackTrace,
 } from './profiling';
 
+type ColumnarCallerCallee = {
+  Label: string[];
+  Value: number[];
+  X: number[];
+  Y: number[];
+  Color: number[];
+};
+
+interface ElasticFlameGraph {
+  Label: string[];
+  Value: number[];
+  Position: number[];
+  Size: number[];
+  Color: number[];
+}
+
 interface PixiFlameGraph extends CallerCalleeNode {
   TotalTraces: number;
   TotalSeconds: number;
+}
+
+const frameTypeToColors = [
+  [0xfd8484, 0xfd9d9d, 0xfeb5b5, 0xfecece],
+  [0xfcae6b, 0xfdbe89, 0xfdcea6, 0xfedfc4],
+  [0xfcdb82, 0xfde29b, 0xfde9b4, 0xfef1cd],
+  [0x6dd0dc, 0x8ad9e3, 0xa7e3ea, 0xc5ecf1],
+  [0x7c9eff, 0x96b1ff, 0xb0c5ff, 0xcbd8ff],
+  [0x65d3ac, 0x84dcbd, 0xa3e5cd, 0xc1edde],
+  [0xd79ffc, 0xdfb2fd, 0xe7c5fd, 0xefd9fe],
+  [0xf98bb9, 0xfaa2c7, 0xfbb9d5, 0xfdd1e3],
+  [0xcbc3e3, 0xd5cfe8, 0xdfdbee, 0xeae7f3],
+];
+
+function frameTypeToRGB(frameType: number, x: number): number {
+  return frameTypeToColors[frameType][x % 4];
+}
+
+function normalizeColor(rgb: number): number[] {
+  return [
+    Math.floor(rgb / 65536) / 255,
+    (Math.floor(rgb / 256) % 256) / 255,
+    (rgb % 256) / 255,
+    1.0,
+  ];
+}
+
+function normalize(n: number, lower: number, upper: number): number {
+  return (n - lower) / (upper - lower);
 }
 
 function checkIfStringHasParentheses(s: string) {
   return /\(|\)/.test(s);
 }
 
-function getFunctionName(frame: StackFrame) {
-  return frame.FunctionName !== '' && !checkIfStringHasParentheses(frame.FunctionName)
-    ? `${frame.FunctionName}()`
-    : frame.FunctionName;
+function getFunctionName(node: CallerCalleeNode) {
+  return node.FunctionName !== '' && !checkIfStringHasParentheses(node.FunctionName)
+    ? `${node.FunctionName}()`
+    : node.FunctionName;
+}
+
+function getExeFileName(node: CallerCalleeNode) {
+  if (node?.ExeFileName === undefined) {
+    return '';
+  }
+  if (node.ExeFileName !== '') {
+    return node.ExeFileName;
+  }
+  switch (node.FrameType) {
+    case 0:
+      return '<unsymbolized frame>';
+    case 1:
+      return 'Python';
+    case 2:
+      return 'PHP';
+    case 3:
+      return 'Native';
+    case 4:
+      return 'Kernel';
+    case 5:
+      return 'JVM/Hotspot';
+    case 6:
+      return 'Ruby';
+    case 7:
+      return 'Perl';
+    case 8:
+      return 'JavaScript';
+    default:
+      return '';
+  }
+}
+
+function getLabel(node: CallerCalleeNode) {
+  if (node.FunctionName !== '') {
+    const sourceFilename = node.SourceFilename;
+    const sourceURL = sourceFilename ? sourceFilename.split('/').pop() : '';
+    return `${getExeFileName(node)}: ${getFunctionName(node)} in ${sourceURL} #${node.SourceLine}`;
+  }
+  return getExeFileName(node);
 }
 
 export class FlameGraph {
@@ -75,84 +160,90 @@ export class FlameGraph {
     this.logger = logger;
   }
 
-  private getExeFileName(exe: any, type: number) {
-    if (exe?.FileName === undefined) {
-      return '';
-    }
-    if (exe.FileName !== '') {
-      return exe.FileName;
-    }
-    switch (type) {
-      case 0:
-        return '<unsymbolized frame>';
-      case 1:
-        return 'Python';
-      case 2:
-        return 'PHP';
-      case 3:
-        return 'Native';
-      case 4:
-        return 'Kernel';
-      case 5:
-        return 'JVM/Hotspot';
-      case 6:
-        return 'Ruby';
-      case 7:
-        return 'Perl';
-      case 8:
-        return 'JavaScript';
-      default:
-        return '';
-    }
-  }
+  private createColumnarCallerCallee(root: CallerCalleeNode): ColumnarCallerCallee {
+    let columnar: ColumnarCallerCallee = {
+      Label: [],
+      Value: [],
+      X: [],
+      Y: [],
+      Color: [],
+    };
+    const queue = [{ x: 0, depth: 1, node: root }];
 
-  // Generates the label for a flamegraph node
-  //
-  // This is slightly modified from the original code in elastic/prodfiler_ui
-  private getLabel(frame: StackFrame, executable: Executable, type: number) {
-    if (frame.FunctionName !== '') {
-      return `${this.getExeFileName(executable, type)}: ${getFunctionName(frame)} in #${
-        frame.LineNumber
-      }`;
-    }
-    return this.getExeFileName(executable, type);
-  }
+    while (queue.length > 0) {
+      const { x, depth, node } = queue.pop()!;
 
-  toElastic() {
-    const leaves = [];
-    let n = 0;
-
-    for (const trace of this.stacktraces.values()) {
-      const path = ['root'];
-      for (let i = 0; i < trace.FrameID.length; i++) {
-        const label = this.getLabel(
-          this.stackframes.get(trace.FrameID[i])!,
-          this.executables.get(trace.FileID[i])!,
-          trace.Type[i]
-        );
-
-        if (label.length === 0) {
-          path.push(trace.FrameID[i]);
-        } else {
-          path.push(label);
-        }
+      if (x === 0 && depth === 1) {
+        columnar.Label.push('root');
+      } else {
+        columnar.Label.push(getLabel(node));
       }
-      const leaf = {
-        id: path[0],
-        value: 1,
-        depth: trace.FrameID.length,
-        pathFromRoot: Object.fromEntries(path.map((item, i) => [i, item])),
-      };
-      leaves.push(leaf);
+      columnar.Value.push(node.Samples);
+      columnar.X.push(x);
+      columnar.Y.push(depth);
+      columnar.Color.push(frameTypeToRGB(node.FrameType, x));
 
-      n++;
-      if (n >= 1000) {
-        // just don't overload the Kibana flamechart
-        break;
+      node.Callees.sort((a: CallerCalleeNode, b: CallerCalleeNode) => b.Samples - a.Samples);
+
+      const xy = [];
+      let delta = 0;
+      for (const callee of node.Callees) {
+        xy.push({ x: x + delta, depth: depth + 1 });
+        delta += callee.Samples;
+      }
+
+      for (let i = node.Callees.length - 1; i >= 0; i--) {
+        queue.push({ x: xy[i].x, depth: xy[i].depth, node: node.Callees[i] });
       }
     }
 
-    return { leaves };
+    return columnar;
+  }
+
+  private createElasticFlameGraph(columnar: ColumnarCallerCallee): ElasticFlameGraph {
+    const graph: ElasticFlameGraph = {
+      Label: [],
+      Value: [],
+      Position: [],
+      Size: [],
+      Color: [],
+    };
+
+    graph.Label = columnar.Label;
+    graph.Value = columnar.Value;
+
+    const maxX = columnar.Value[0];
+    const maxY = columnar.Y.reduce((max, n) => (n > max ? n : max), 0);
+
+    for (let i = 0; i < columnar.X.length; i++) {
+      const x = normalize(columnar.X[i], 0, maxX);
+      const y = normalize(maxY - columnar.Y[i], 0, maxY);
+      graph.Position.push(x, y);
+    }
+
+    graph.Size = graph.Value.map((n) => normalize(n, 0, maxX));
+
+    for (const color of columnar.Color) {
+      graph.Color.push(...normalizeColor(color));
+    }
+
+    return graph;
+  }
+
+  toElastic(): ElasticFlameGraph {
+    const rootFrame = createStackFrameMetadata();
+    const frameMetadataForTraces = groupStackFrameMetadataByStackTrace(
+      this.stacktraces,
+      this.stackframes,
+      this.executables
+    );
+    const diagram = createCallerCalleeIntermediateRoot(
+      rootFrame,
+      this.events,
+      frameMetadataForTraces
+    );
+    const root = fromCallerCalleeIntermediateNode(diagram);
+    return this.createElasticFlameGraph(this.createColumnarCallerCallee(root));
   }
 
   toPixi(): PixiFlameGraph {
